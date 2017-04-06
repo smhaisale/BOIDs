e0deebb98ae6ccc908d10a90c5dc624d1bedceb6
Shahbaaz <shahbaaz@cmu.edu>
2017-04-06 16:00:12 -0700

    Added core files and DroneParams

---
+++ go-boids/DroneParams.go
@@ -0,0 +1,22 @@
+package go_boids
+
+type Position struct {
+    X, Y, Z float64                 // 3D position of the drone
+}
+
+type Speed struct {
+    vX, vY, vZ float64              // Dimensional velocities
+    speed float64                   // The actual velocity
+}
+
+type Dimensions struct {
+    dX, dY, dZ float64              // Any set of X, Y and Z dimensions
+}
+
+type DroneType struct {
+    typeId string                   // Identifier for a type
+    typeDescription string          // Description
+    size Dimensions                 // Size of this drone type
+    maxRange Dimensions             // Communication with another drone fails outside this range
+    maxSpeed Speed                  // Maximum speed for this drone type
+}
