41679f4bbe9811ee37dc49aac90a146ab2d12450
lyfjoey <lyfyueye@gmail.com>
2017-04-12 14:45:52 -0700

    interaction changed

--- main/Interaction.go
+++ main/Interaction.go
@@ -7,7 +7,7 @@ func main() {
 	fmt.Scanf("%s", &name)
 	Listen(NodeMap[name].port)
 	fmt.Println("listening")
-	myDrone := Drone{Position{0, 1, 2}, DroneType{"0", "normal", Dimensions{1, 2, 3}, Dimensions{1, 2, 3}, Speed{1, 2, 3}}, Speed{1, 2, 3}}
+	myDrone := Drone{"0", Position{0, 1, 2}, DroneType{"0", "normal", Dimensions{1, 2, 3}, Dimensions{1, 2, 3}, Speed{1, 2, 3}}, Speed{1, 2, 3}}
 	msgData := MessageData{[]Drone{myDrone}}
 	msg := TcpMessage{"alice", "bob", 0, false, "Message", VectorTime{map[string]int{"alice": 1}}, msgData, MulticastData{}}
 	var input int
