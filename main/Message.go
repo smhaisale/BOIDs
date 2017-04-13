3b12d9b290161ebbec3ee7fa0029afd8c4395a3a
lyfjoey <lyfyueye@gmail.com>
2017-04-12 18:33:23 -0700

    Message fields with Capital letter

--- main/Message.go
+++ main/Message.go
@@ -3,27 +3,27 @@ package main
 // Message structure used for TCP connections between drones
 // Contains message metadata and payload
 type TcpMessage struct {
-    source          string          `json: "source"`
-    destination     string          `json: "destination"`
-    seqNum          int             `json: "seqNum"`
-    duplicate       bool            `json: "duplicate"`
-    msgType            string          `json: "type"`
-    timestamp       VectorTime      `json: "vectorTimestamp"`
-    data            MessageData     `json: "data"`
-    multicastData   MulticastData   `json: "multicastData"`
+    Source          string          `json: "source"`
+    Destination     string          `json: "destination"`
+    SeqNum          int             `json: "seqNum"`
+    Duplicate       bool            `json: "duplicate"`
+    MsgType            string          `json: "msgType"`
+    Timestamp       VectorTime      `json: "vectorTimestamp"`
+    Data            MessageData     `json: "data"`
+    MulticastData   MulticastData   `json: "multicastData"`
 }
 
 // Contains message payload
 type MessageData struct {
-    drones          []Drone         `json: "drones"`
+    Drones          []Drone         `json: "drones"`
 }
 
 // Contains metadata specific to multicast messages
 type MulticastData struct {
-    source          string          `json: "source"`
-    destination     string          `json: "destination"`
-    groupSeqNum     int             `json: "groupSeqNum"`
-    groupName       string          `json: "groupName"`
+    Source          string          `json: "source"`
+    Destination     string          `json: "destination"`
+    GroupSeqNum     int             `json: "groupSeqNum"`
+    GroupName       string          `json: "groupName"`
 }
 
 var sampleMulticastData = MulticastData {"multicastSource", "multicastDestination",
