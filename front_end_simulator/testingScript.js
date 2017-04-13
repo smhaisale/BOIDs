9dd85f871467058890bc852d8f0aa7586e7433f1
Shahbaaz <shahbaaz@cmu.edu>
2017-04-13 16:33:12 -0700

    Added functionality to add and kill drones, pause and debug visualization

--- front_end_simulator/testingScript.js
+++ front_end_simulator/testingScript.js
@@ -1,22 +1,22 @@
 var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
-                       window.mozRequestAnimationFrame || window.msRequestAnimationFrame || 
-                       function(c) {window.setTimeout(c, 15)};
+    window.mozRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(c) {window.setTimeout(c, 15)};
 /**
-   Phoria
-   pho·ri·a (fôr-)
-   n. The relative directions of The Eyes during binocular fixation on a given object
-*/
+ Phoria
+ pho·ri·a (fôr-)
+ n. The relative directions of The Eyes during binocular fixation on a given object
+ */
 
 // bind to window onload event
 window.addEventListener('load', onloadHandler, false);
 
 var testData = [{"ID":"drone1","pos":{"X":0,"Y":0,"Z":0},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}},
-                {"ID":"drone2","pos":{"X":0,"Y":0,"Z":0},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}},
-                {"ID":"drone3","pos":{"X":0,"Y":0,"Z":0},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}}];
+    {"ID":"drone2","pos":{"X":0,"Y":0,"Z":0},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}},
+    {"ID":"drone3","pos":{"X":0,"Y":0,"Z":0},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}}];
 
 var testData2 = [{"ID":"drone1","pos":{"X":-1,"Y":-1,"Z":-1},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}},
-                {"ID":"drone2","pos":{"X":-2,"Y":-2,"Z":-2},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}},
-                {"ID":"drone3","pos":{"X":-3,"Y":-3,"Z":-3},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}}];
+    {"ID":"drone2","pos":{"X":-2,"Y":-2,"Z":-2},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}},
+    {"ID":"drone3","pos":{"X":-3,"Y":-3,"Z":-3},"type":{"TypeId":"type1","TypeDescription":"Simple sample drone type","Size":{"DX":1,"DY":1,"DZ":1},"MaxRange":{"DX":10,"DY":10,"DZ":10},"MaxSpeed":{"VX":10,"VY":10,"VZ":10}},"speed":{"VX":2,"VY":1,"VZ":-1}}];
 
 var bitmaps = [];
 var scene = new Phoria.Scene();
@@ -24,60 +24,88 @@ var sphereList = [];
 var droneList = [];
 
 var pause = true;
+var debug = false;
 
 var drone1 = new Drone();
 
 function createSphere(id, size, x, y, z) {
 
-   var s = Phoria.Util.generateSphere(size, 24, 48);
+    var s = Phoria.Util.generateSphere(size, 24, 48);
 
-   var offsetPoints = [];
+    var offsetPoints = [];
 
-   for(var pointNumber = 0; pointNumber < s.points.length; pointNumber++) {
-       offsetPoints.push({
-           x: s.points[pointNumber].x + x,
-           y: s.points[pointNumber].y + y,
-           z: s.points[pointNumber].z + z
-       });
-   }
+    for(var pointNumber = 0; pointNumber < s.points.length; pointNumber++) {
+        offsetPoints.push({
+            x: s.points[pointNumber].x + x,
+            y: s.points[pointNumber].y + y,
+            z: s.points[pointNumber].z + z
+        });
+    }
 
-   return Phoria.Entity.create({
-       id: id,
-       points: offsetPoints,
-       edges: s.edges,
-       polygons: s.polygons,
-       style: {
-           diffuse: 1,
-           specular: 128
-       }
-   });
+    return Phoria.Entity.create({
+        id: id,
+        points: offsetPoints,
+        edges: s.edges,
+        polygons: s.polygons,
+        style: {
+            diffuse: 1,
+            specular: 128
+        }
+    });
 }
 
 function makeSphereWithValue() {
-      var input = document.getElementById('values');
-      var data = input.value.split(",");
-      //console.log("making sphere at " +  data[0] + "," + data[1] + "," + data[2]);
-      for (var i = 0; i < droneList.length; i++) {
-         droneList[i].newX = data[0];
-         droneList[i].newY = data[1];
-         droneList[i].newZ = data[2];
-         droneList[i].dX = (droneList[i].newX - droneList[i].currentX) * droneList[i].speed;
-         droneList[i].dY = (droneList[i].newY - droneList[i].currentY) * droneList[i].speed;
-         droneList[i].dZ = (droneList[i].newZ - droneList[i].currentZ) * droneList[i].speed;
-         pause = false;
+    var input = document.getElementById('values');
+    var data = input.value.split(",");
+    //console.log("making sphere at " +  data[0] + "," + data[1] + "," + data[2]);
+    for (var i = 0; i < droneList.length; i++) {
+        droneList[i].newX = data[0];
+        droneList[i].newY = data[1];
+        droneList[i].newZ = data[2];
+        droneList[i].dX = (droneList[i].newX - droneList[i].currentX) * droneList[i].speed;
+        droneList[i].dY = (droneList[i].newY - droneList[i].currentY) * droneList[i].speed;
+        droneList[i].dZ = (droneList[i].newZ - droneList[i].currentZ) * droneList[i].speed;
+        pause = false;
 
-      }
+    }
 
-      //var sphere = createSphere(parseFloat(data[0]), parseInt(data[1]), parseInt(data[2]), parseInt(data[3]));
-      //sphereList.push(sphereList);
-      //scene.graph.push(sphere);
+    //var sphere = createSphere(parseFloat(data[0]), parseInt(data[1]), parseInt(data[2]), parseInt(data[3]));
+    //sphereList.push(sphereList);
+    //scene.graph.push(sphere);
 }
-
 
 //Used in frontend to refresh drone positions
 function flipPause() {
-   pause = !pause;
-   console.log("Paused: " + pause);
+    pause = !pause;
+    if (pause) {
+        document.getElementById("flipPause").innerHTML = "Resume";
+    } else {
+        document.getElementById("flipPause").innerHTML = "Pause";
+    }
+    console.log("Paused: " + pause);
+}
+
+//Used in frontend to refresh drone positions
+function flipDebug() {
+    debug = !debug;
+    if (debug) {
+        for (var i = 0; i < droneList.length; i++) {
+            Phoria.Entity.debug(droneList[i].sphere, {
+                showId: true,
+                showPosition: true
+            });
+        }
+        document.getElementById("flipDebug").innerHTML = "Remove Debug Info";
+    } else {
+        for (var i = 0; i < droneList.length; i++) {
+            Phoria.Entity.debug(droneList[i].sphere, {
+                showId: false,
+                showPosition: false
+            });
+        }
+        document.getElementById("flipDebug").innerHTML = "Add Debug Info";
+    }
+    console.log("Debug: " + debug);
 }
 
 function loadDroneData() {
@@ -89,10 +117,10 @@ function loadDroneData() {
         sphereList.push(sphere);
         scene.graph.push(sphere);
 
-        Phoria.Entity.debug(sphere, {
-            showId: true,
-            showPosition: true
-        });
+        //Phoria.Entity.debug(sphere, {
+        //    showId: true,
+        //    showPosition: true
+        //});
 
         drone.sphere = sphere;
         droneList.push(drone);
@@ -129,22 +157,22 @@ function updateDronePositions() {
 
 function onloadHandler()
 {
-   loadDroneData();
-   // get conference list
+    loadDroneData();
+    // get conference list
 
-   // get the images loading
-   var loader = new Phoria.Preloader();
-   for (var i=0; i<6; i++)
-   {
-      bitmaps.push(new Image());
-      loader.addImage(bitmaps[i], 'images/texture'+i+'.png');
-   }
-   loader.onLoadCallback(init);
+    // get the images loading
+    var loader = new Phoria.Preloader();
+    for (var i=0; i<6; i++)
+    {
+        bitmaps.push(new Image());
+        loader.addImage(bitmaps[i], 'images/texture'+i+'.png');
+    }
+    loader.onLoadCallback(init);
 }
 
 function init()
 {
-   console.log("init()");
+    console.log("init()");
 
     var Rnd = function(s) {return Math.random() * s};
 
@@ -169,19 +197,19 @@ function init()
     // create a canvas renderer
     var renderer = new Phoria.CanvasRenderer(canvas);
     // add a grid to help visualise camera position etc.
-   var plane = Phoria.Util.generateTesselatedPlane(16,16,0,40);
-   scene.graph.push(Phoria.Entity.create({
-      points: plane.points,
-      edges: plane.edges,
-      polygons: plane.polygons,
-      style: {
-         //color: [160,255,160],
-         drawmode: "wireframe",
-         shademode: "plain",
-         linewidth: 0.5,
-         objectsortmode: "back"
-      }
-   }));
+    var plane = Phoria.Util.generateTesselatedPlane(16,16,0,40);
+    scene.graph.push(Phoria.Entity.create({
+        points: plane.points,
+        edges: plane.edges,
+        polygons: plane.polygons,
+        style: {
+            //color: [160,255,160],
+            drawmode: "wireframe",
+            shademode: "plain",
+            linewidth: 0.5,
+            objectsortmode: "back"
+        }
+    }));
 
     var fnGenerateStarfield = function(num, scale) {
         scale = scale || 1;
@@ -211,74 +239,72 @@ function init()
         vec4.transformMat4(position, position, rotMatrix);
     });
 
+    scene.graph.push(Phoria.DistantLight.create({
+        direction: {x:0, y:-0.5, z:1}
+    }));
 
-    //scene.graph.push(cube);
-   scene.graph.push(Phoria.DistantLight.create({
-      direction: {x:0, y:-0.5, z:1}
-   }));
+    var fnAnimate = function() {
+        if (!pause)
+        {
+            for (var i = 0; i < droneList.length; i++) {
+                var drone = droneList[i];
+                var sphere = drone.sphere;
+                sphere.translateX(drone.dX);
+                sphere.translateY(drone.dY);
+                sphere.translateZ(drone.dZ);
 
-   var fnAnimate = function() {
-      if (!pause)
-      {
-         for (var i = 0; i < droneList.length; i++) {
-            var drone = droneList[i];
-            var sphere = drone.sphere;
-            sphere.translateX(drone.dX);
-            sphere.translateY(drone.dY);
-            sphere.translateZ(drone.dZ);
+                drone.currentX += drone.dX;
+                drone.currentY += drone.dY;
+                drone.currentZ += drone.dZ;
 
-            drone.currentX += drone.dX;
-            drone.currentY += drone.dY;
-            drone.currentZ += drone.dZ;
-
-            if ((Math.abs(drone.currentX - drone.newX) < 0.001) && (Math.abs(drone.currentY - drone.newY) < 0.001) && (Math.abs(drone.currentZ - drone.newZ) < 0.001)) {
-               drone.dX = 0;
-               drone.dY = 0;
-               drone.dZ = 0;
+                if ((Math.abs(drone.currentX - drone.newX) < 0.001) && (Math.abs(drone.currentY - drone.newY) < 0.001) && (Math.abs(drone.currentZ - drone.newZ) < 0.001)) {
+                    drone.dX = 0;
+                    drone.dY = 0;
+                    drone.dZ = 0;
+                }
             }
-         }
-      }
-      scene.modelView();
-      renderer.render(scene);
-      requestAnimFrame(fnAnimate);
-   };
+        }
+        scene.modelView();
+        renderer.render(scene);
+        requestAnimFrame(fnAnimate);
+    };
 
-   // key binding
-   document.addEventListener('keydown', function(e) {
-      if (e.keyCode == 27)
-      {
-        console.log("pausing");
-        pause = !pause;
-      }
-   }, false);
+    // key binding
+    document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 27)
+        {
+            console.log("pausing");
+            pause = !pause;
+        }
+    }, false);
 
-   /*
-   KEY:
-   {
-      SHIFT:16, CTRL:17, ESC:27, RIGHT:39, UP:38, LEFT:37, DOWN:40, SPACE:32,
-      A:65, E:69, G:71, L:76, P:80, R:82, S:83, Z:90
-   },
-   */
+    /*
+     KEY:
+     {
+     SHIFT:16, CTRL:17, ESC:27, RIGHT:39, UP:38, LEFT:37, DOWN:40, SPACE:32,
+     A:65, E:69, G:71, L:76, P:80, R:82, S:83, Z:90
+     },
+     */
 
-   // add GUI controls
-   var gui = new dat.GUI();
-   var f = gui.addFolder('Perspective');
-   f.add(scene.perspective, "fov").min(5).max(175);
-   f.add(scene.perspective, "near").min(1).max(100);
-   f.add(scene.perspective, "far").min(1).max(1000);
-   //f = gui.addFolder('Camera LookAt');
-   //f.add(scene.camera.lookat, "x").min(-100).max(100);
-   //f.add(scene.camera.lookat, "y").min(-100).max(100);
-   //f.add(scene.camera.lookat, "z").min(-100).max(100);
-   f = gui.addFolder('Camera Position');
-   f.add(scene.camera.position, "x").min(-100).max(100);
-   f.add(scene.camera.position, "y").min(-100).max(100);
-   f.add(scene.camera.position, "z").min(-100).max(100);
-   f = gui.addFolder('Camera Up');
-   f.add(scene.camera.up, "x").min(-10).max(10).step(0.1);
-   f.add(scene.camera.up, "y").min(-10).max(10).step(0.1);
-   f.add(scene.camera.up, "z").min(-10).max(10).step(0.1);
+    // add GUI controls
+    var gui = new dat.GUI();
+    var f = gui.addFolder('Perspective');
+    f.add(scene.perspective, "fov").min(5).max(175);
+    f.add(scene.perspective, "near").min(1).max(100);
+    f.add(scene.perspective, "far").min(1).max(1000);
+    //f = gui.addFolder('Camera LookAt');
+    //f.add(scene.camera.lookat, "x").min(-100).max(100);
+    //f.add(scene.camera.lookat, "y").min(-100).max(100);
+    //f.add(scene.camera.lookat, "z").min(-100).max(100);
+    f = gui.addFolder('Camera Position');
+    f.add(scene.camera.position, "x").min(-100).max(100);
+    f.add(scene.camera.position, "y").min(-100).max(100);
+    f.add(scene.camera.position, "z").min(-100).max(100);
+    f = gui.addFolder('Camera Up');
+    f.add(scene.camera.up, "x").min(-10).max(10).step(0.1);
+    f.add(scene.camera.up, "y").min(-10).max(10).step(0.1);
+    f.add(scene.camera.up, "z").min(-10).max(10).step(0.1);
 
-   // start animation
-   requestAnimFrame(fnAnimate);
+    // start animation
+    requestAnimFrame(fnAnimate);
 }
