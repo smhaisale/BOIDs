b0f8256cfc9159023a962accca11f328dc42aac3
ishikab <ishikabtr@gmail.com>
2017-04-27 01:03:15 -0700

    Bug fix

--- main/Environment.go
+++ main/Environment.go
@@ -74,9 +74,16 @@ func addDrone(w http.ResponseWriter, r *http.Request) {
 func killDrone(w http.ResponseWriter, r *http.Request) {
     address := r.URL.Query().Get("data")
     log.Println("Received kill drone request at address " + address)
-    for _, swarmDrone := range droneMap {
-        killDroneAddress := "http://" + swarmDrone.Address + DRONE_KILL_DRONE_URL + "?address=" + address
-        makeGetRequest(killDroneAddress, "")
+    killDrone, err := getDroneFromServer(address)
+    if err != nil {
+        log.Println("Error! ", err)
+        return
+    } else {
+        delete(droneMap, killDrone.ID)
+        for _, swarmDrone := range droneMap {
+            killDroneAddress := "http://" + swarmDrone.Address + DRONE_KILL_DRONE_URL + "?address=" + address
+            makeGetRequest(killDroneAddress, "")
+        }
     }
     w.Header().Set("Access-Control-Allow-Origin", "*")
 }
